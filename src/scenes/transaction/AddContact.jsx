343b44b2bb4a9a324298ad8a0221bebe730c5fa1
qibin1227 <qibin041227@gmail.com>
2024-05-08 21:13:45 +0800

    bbb

--- src/scenes/transaction/AddContact.jsx
+++ src/scenes/transaction/AddContact.jsx
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import { Box, TextField, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./AddContact.css";
+import axios from "axios";
+import { useAuth } from "../../AuthProvider";
 
 const AddContact = ({ onFinish }) => {
+  const { userData } = useAuth();
   const [nickName, setNickName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [accountNumber, setAccountNumber] = useState("");
+
+
   const navigate = useNavigate();
 
-  const handleFinish = () => {
+  const handleFinish = async () => {
     // Validate the fields
     if (!nickName || nickName.trim() === "") {
       alert("Please enter a nickname.");
@@ -23,13 +28,45 @@ const AddContact = ({ onFinish }) => {
       alert("Please enter an account number.");
       return;
     }
+
+    const contactList= {
+      user_created_name: nickName,
+      account_number: accountNumber,
+      contact_mobile: phoneNumber
+    };
+
+    
+     
     // Add the contact to the contact list database
     // Replace this with your logic to add the contact
-    console.log(
-      `Adding contact: ${nickName}, ${phoneNumber}, ${accountNumber}`
-    );
+    // console.log(
+    //   `Adding contact: ${nickName}, ${phoneNumber}, ${accountNumber}`
+    // );
+
+    
+    axios.post(`http://localhost:8080/ContactList/${userData.username}/add`, contactList)
+  .then(response => {
+    // Handle successful response
+    console.log(response.data); // You can access response data here if needed
+    console.log("Successfully Add")
+    navigate("/transaction/transfer",  { state: { accountNumber } })
+  })
+  .catch(error => {
+    // Handle errors
+    if (error.response.status === 502 ) {
+      alert("The User is already Exist in Your Contact List");
+    } else {
+      // Handle other errors
+      alert("The User is not yet Registered");
+      // You can show a generic error message or handle it as per your requirement
+    }
+  });
+
+    
     // Redirect to Transfer.jsx after adding the contact
-    onFinish();
+   
+
+
   };
 
   return (
